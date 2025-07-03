a0dab28fddbce2cb4a28d876e682c017811442e7
infinitecomm <crissmaina32@gmail.com>
2025-07-03 21:12:09 +0300

    dimensions

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { StyleSheet,View,} from 'react-native';
-import Box from './components/box';
+import { StyleSheet,View,Text, Dimensions} from 'react-native';
 
 
 export default function App() {
@@ -8,27 +7,38 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-     <Box style={{ backgroundColor: 'red' }}>Box1</Box>
-      <Box style={{ backgroundColor: 'blue' }}>Box2</Box>
-      <Box style={{ backgroundColor: 'yellow' }}>Box3</Box>
-      <Box style={{ backgroundColor: 'purple' }}>Box4</Box>
-      <Box style={{ backgroundColor: 'aqua' }}>Box5</Box>
-      <Box style={{ backgroundColor: 'green' }}>Box6</Box>
-      <Box style={{ backgroundColor: 'wheat' }}>Box7</Box>
-      <Box style={{ backgroundColor: 'orange' }}>Box8</Box>
+      <View style={styles.box}>
+        <Text style={styles.text}>
+          Welcome!
+        </Text>
+
+      </View>
+     
     </View>
   );
 }
 
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
+
 const styles = StyleSheet.create({
   container: {
-    // flex:1,
-    flexWrap:'wrap',
-    columnGap:10,
-    height:300,
-    marginTop:40,
-    borderWidth:6,
-    padding:12,
-    borderColor:'red'
+    flex:1,
+    backgroundColor:'plum',
+    alignItems: 'center',
+    justifyConten:'center',
+        paddingTop:40,
+
   },
+   box:{
+    width: windowWidth > 500 ? "70%": "90%",
+    height:windowHeight > 600? "30%": "90%",
+    backgroundColor:'wheat',
+    alignItems: 'center',
+    justifyContent:'center'
+   },
+   text:{
+    fontSize:windowWidth > 500 ? 40 : 24,
+   }
 });
