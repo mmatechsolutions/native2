557b143fcd800413bdf976518ff29a672e0318d1
infinitecomm <crissmaina32@gmail.com>
2025-07-21 10:43:17 +0300

    made cards

--- App.js
+++ App.js
@@ -1,13 +1,78 @@
 import React from 'react';
-import { StyleSheet,SafeAreaView, Platform} from 'react-native';
+import { StyleSheet,SafeAreaView, Platform, ScrollView} from 'react-native';
 import CarCard from './components/carCard';
 
 
 export default function App() {
+  const cars = {
+    "name": "Audi RS7",
+    "top_speed": "305 km/h",
+    "color": "Nardo Gray",
+    "fuel": "Petrol",
+    "transmission": "8-speed automatic",
+    "image": require("./assets/audi.jpg")
+ }
+ const gtr={
+    "name": "Nissan GT-R",
+    "top_speed": "315 km/h",
+    "color": "Jet Black Pearl",
+    "fuel": "Petrol",
+    "transmission": "6-speed dual-clutch",
+    "image":  require("./assets/gtr.jpg")
+  }
+ const porsche = {
+    "name": "Porsche 911",
+    "top_speed": "330 km/h",
+    "color": "Guards Red",
+    "fuel": "Petrol",
+    "transmission": "8-speed PDK automatic",
+    "image": require("./assets/911.jpg")
+  }
+
+ const m5= {
+    "name": "BMW M5",
+    "top_speed": "305 km/h",
+    "color": "Marina Bay Blue",
+    "fuel": "Petrol",
+    "transmission": "8-speed automatic",
+    "image":require("./assets/m5.jpg")
+  }
+  const benz={
+    "name": "Mercedes-AMG GT",
+    "top_speed": "318 km/h",
+    "color": "Designo Selenite Grey",
+    "fuel": "Petrol",
+    "transmission": "7-speed dual-clutch automatic",
+    "image": require("./assets/benz.jpg")
+  }
+  const lambo={
+    "name": "Lamborghini Huracán",
+    "top_speed": "325 km/h",
+    "color": "Verde Mantis",
+    "fuel": "Petrol",
+    "transmission": "7-speed dual-clutch",
+    "image": require("./assets/lambo.jpg")
+  }
+ const corvette= {
+    "name": "Chevrolet Corvette C8",
+    "top_speed": "312 km/h",
+    "color": "Torch Red",
+    "fuel": "Petrol",
+    "transmission": "8-speed dual-clutch",
+    "image":require("./assets/corvette.jpg")
+  }
  
   return (
       <SafeAreaView style={styles.container}>
-        <CarCard/>
+        <ScrollView>
+          <CarCard {...cars}/>
+          <CarCard {...porsche}/>
+          <CarCard {...gtr}/>
+          <CarCard {...m5}/>
+          <CarCard {...lambo}/>
+          <CarCard {...benz}/>
+          <CarCard {...corvette}/>
+        </ScrollView>
     </SafeAreaView>
   );
 }
@@ -18,7 +83,7 @@ const styles = StyleSheet.create({
   
   container: {
     flex:1,
-    backgroundColor:'f5f5',
+    backgroundColor:'f5f5f5',
     paddingTop: Platform.OS === "android" ? 50:0,
   }
 });
--- components/carCard.js
+++ components/carCard.js
@@ -1,9 +1,98 @@
- import { View,Text } from "react-native";
+ import { View,Text, StyleSheet,Platform,Image} from "react-native";
 
- export default function CarCard(){
+ export default function CarCard( 
+    {   name,
+        top_speed,
+        color,
+        fuel,
+        transmission,
+        image}
+ ){
      return(
-        <View>
-            <Text>Car Card</Text>
+        <View style={styles.card}>
+           <View style={styles.nameContainer}>
+            <Text style={styles.name}>{name}</Text>
+            <Text style={styles.color}>{color}</Text>
+           </View>
+
+           <Image 
+           source={image}
+            accessibilityLabel={`${name}`} 
+            style={styles.image}
+            />
+
+            <View style={styles.specsContainer}>
+            <View style={styles.specItem}>
+                <Text style={styles.label}>Fuel:</Text>
+                <Text style={styles.value}>{fuel}</Text>
+            </View>
+
+            <View style={styles.specItem}>
+                <Text style={styles.label}>Top Speed:</Text>
+                <Text style={styles.value}>{top_speed}</Text>
+            </View>
+
+            <View style={styles.specItem}>
+                <Text style={styles.label}>Transmission:</Text>
+                <Text style={styles.value}>{transmission}</Text>
+            </View>
+            </View>
+
         </View>
      )
  }
+
+ const styles = StyleSheet.create({
+    card: {
+        backgroundColor:'wheat',
+        borderRadius:16,
+        borderWidth:2,
+        padding:16,
+        margin:16,
+        ...Platform.select({
+           ios: {
+                shadowOffset:{width:2, height:2},
+                shadowColor:"#333",
+                shadowOpacity:0.3,
+                shadowRadius:4
+            },
+            android:{
+                elevation:5
+            }
+    })
+    },
+    nameContainer:{
+        flexDirection:"row",
+        justifyContent:"space-between",
+        marginBottom:32
+    },
+    name:{
+        fontSize:30,
+        fontWeight:"bold"
+    },
+    color:{
+        fontSize:22
+    },
+    image:{
+        width:'100%',
+        height: 200,
+        marginBottom:16
+    },
+    specsContainer: {
+        marginTop: 10,
+        gap: 8 
+    },
+    specItem: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    label: {
+        fontWeight: 'bold',
+        marginRight: 6,
+        color: '#333'
+    },
+    value: {
+        color: '#555'
+    }
+
+ })
