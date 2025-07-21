c5b63e0bfaf9fdd6a01181f4b66e5f333ee902d4
infinitecomm <crissmaina32@gmail.com>
2025-07-21 13:22:10 +0300

    flatlist

--- components/carCard.js
+++ components/carCard.js
@@ -1,98 +1,70 @@
- import { View,Text, StyleSheet,Platform,Image} from "react-native";
+ import { View,Text, StyleSheet,Platform, FlatList} from "react-native";
+ import carData from "./cars.json"
 
- export default function CarCard( 
-    {   name,
-        top_speed,
-        color,
-        fuel,
-        transmission,
-        image}
- ){
+ export default function CarCard(){
      return(
-        <View style={styles.card}>
-           <View style={styles.nameContainer}>
-            <Text style={styles.name}>{name}</Text>
-            <Text style={styles.color}>{color}</Text>
-           </View>
-
-           <Image 
-           source={image}
-            accessibilityLabel={`${name}`} 
-            style={styles.image}
-            />
-
-            <View style={styles.specsContainer}>
-            <View style={styles.specItem}>
-                <Text style={styles.label}>Fuel:</Text>
-                <Text style={styles.value}>{fuel}</Text>
-            </View>
-
-            <View style={styles.specItem}>
-                <Text style={styles.label}>Top Speed:</Text>
-                <Text style={styles.value}>{top_speed}</Text>
-            </View>
-
-            <View style={styles.specItem}>
-                <Text style={styles.label}>Transmission:</Text>
-                <Text style={styles.value}>{transmission}</Text>
-            </View>
-            </View>
-
-        </View>
+        <FlatList 
+        data={carData.cars}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({item})=>{
+            return(
+                <View style={styles.nameContainer}>
+                    <Text style={styles.name}>{item.name}</Text>
+                    <Text style={styles.speed}>{item.top_speed}</Text>
+                    <Text style={styles.color}>{item.color}</Text>
+                </View>
+            )
+        }}
+        ItemSeparatorComponent={<View style={{height:20}}/>}
+        ListEmptyComponent={<Text>No Cars Found</Text>}
+        ListHeaderComponent={<Text style={styles.header}>Cars</Text>}
+        ListFooterComponent={<Text style={styles.header}>End of Cars</Text>}
+        />
      )
  }
 
  const styles = StyleSheet.create({
-    card: {
-        backgroundColor:'wheat',
-        borderRadius:16,
-        borderWidth:2,
-        padding:16,
-        margin:16,
+    nameContainer: {
+        backgroundColor: '#fff',
+        padding: 16,
+        borderRadius: 12,
+        marginHorizontal: 16,
+        // marginBottom: 12,
+        borderWidth: 1,
+        borderColor: '#ccc',
         ...Platform.select({
-           ios: {
-                shadowOffset:{width:2, height:2},
-                shadowColor:"#333",
-                shadowOpacity:0.3,
-                shadowRadius:4
+            ios: {
+            shadowColor: '#000',
+            shadowOffset: { width: 2, height: 2 },
+            shadowOpacity: 0.1,
+            shadowRadius: 4,
             },
-            android:{
-                elevation:5
+            android: {
+            elevation: 3,
             }
-    })
-    },
-    nameContainer:{
-        flexDirection:"row",
-        justifyContent:"space-between",
-        marginBottom:32
-    },
-    name:{
-        fontSize:30,
-        fontWeight:"bold"
-    },
-    color:{
-        fontSize:22
-    },
-    image:{
-        width:'100%',
-        height: 200,
-        marginBottom:16
-    },
-    specsContainer: {
-        marginTop: 10,
-        gap: 8 
-    },
-    specItem: {
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    label: {
+        })
+        },
+    name: {
+        fontSize: 20,
         fontWeight: 'bold',
-        marginRight: 6,
-        color: '#333'
-    },
-    value: {
+        color: '#333',
+        marginBottom: 6
+        },
+    speed: {
+        fontSize: 16,
+        color: '#0077cc',
+        marginBottom: 4
+        },
+    color: {
+        fontSize: 16,
         color: '#555'
-    }
+        },
+
+        header:{
+            fontSize:24,
+            fontWeight:'bold',
+            textAlign:'center',
+            margin:12
+        }
 
  })
